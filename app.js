5f65f79f14811c3acf2c8ff16f4bc1164fa696b9
Ambuj <thecoderguy@outlook.com>
2018-10-07 11:01:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get("/results", function(req, res){
     var search = req.query.query;
     var ab = search.split(" ");
     search = ab.join("+");
-    var url = 'https://pixabay.com/api/?key=9494342-0f51f6ed3422203dd259dafaa&q='+search+'&image_type=photo&order=latest';
+    var url = 'https://pixabay.com/api/?key=9494342-0f51f6ed3422203dd259dafaa&q='+search+'&image_type=photo&order=latest&per_page=35';
     request(url, function(error, response, body){
      if(!error && response.statusCode == 200){
        var parsed = JSON.parse(body);
